088167c630d668b6bbcdc9b389b482de292a0d4c
Vadikgoyal <vypergamer111@gmail.com>
2022-06-13 18:14:51 +0530

    vadikgoyal
    A Web page made with html, css, and js( java script )

--- About/js/script.js
+++ About/js/script.js
@@ -2,9 +2,16 @@ burger = document.querySelector('.burger')
 navbar = document.querySelector('.navbar')
 navList = document.querySelector('.nav-list')
 rightNav = document.querySelector('.rightNav')
+loader = document.querySelector('preloader')
 
 burger.addEventListener('click', ()=>{
 	rightNav.classList.toggle('v-class')
 	navList.classList.toggle('v-class')
 	navbar.classList.toggle('h-nav')
 })
+
+var loader = document.getElementById('preloader')
+
+window.addEventListener("load", function(){
+	loader.style.display = "none";
+	})
